e2457f4d5e9b1860ea7af3e88b5c1f9008e4167c
matheusalmeida01 <matheus.almeida@think.br.com>
2023-03-01 15:24:36 -0300

    seguir usuario

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -153,3 +153,17 @@ func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 
 	return usuario, nil
 }
+
+func (repositorio Usuarios) Seguir(usuarioID, seguidoID uint64) error {
+	statement, erro := repositorio.db.Prepare("insert ignore into seguidores (usuario_id, seguidor_id) values (?, ?)")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
